2d7a8154622cea42e5ed0a4b1a557a6dc551178d
samuelgoodnews15@gmail.com <samuelgoodnews15@gmail.com>
2023-09-08 06:21:09 +0100

    updated time format

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@ header("content-type:application/json");
 header("Access-Control-Allow-Method:GET");
 header("Access-Control-Allow-Origin:*");
 
-// validating the incoming request 
+// validate request 
 
 	if (isset($_GET['slack_name']) && $_GET['slack_name'] !== "" && isset($_GET['track']) && $_GET['track'] !== "") {
 		response($_GET['slack_name'], $_GET['track'], 200);	
@@ -23,10 +23,11 @@ function response($slack_name, $track, $response_code)
 	if ($response_code === 200) {
 		$response['slack_name'] = $slack_name;
 		$response['current_day'] = $dayOfWeek;
-		$response['utc_time'] =  date("Y-m-d H:i:s", time());
+		// $response['utc_time'] =  date("Y-m-d H:i:s", time());
+		$response['utc_time'] =  gmdate("Y-m-d\TH:i:s\Z");
 		$response['track'] =  $track;
-		$response['github_file_url'] =  "https://github.com/emekaenyinnia/HNGx-stage-one/blob/main/api/index.php";
-		$response['github_repo_url'] =  "https://github.com/emekaenyinnia/HNGx-stage-one";
+		$response['github_file_url'] =  "https://github.com/GoodnewsPerfect/HNGx-task1/blob/main/api/index.php";
+		$response['github_repo_url'] =  "https://github.com/GoodnewsPerfect/HNGx-task1";
 		$response['status_code'] =  $response_code;
 	
 	} else{
